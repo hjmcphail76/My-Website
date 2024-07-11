a389a515327eb1e54b68cb062c435674da7c37c4
hjmcphail76 <hjmcphail@gmail.com>
2024-07-11 11:41:14 -0400

    done! Lots of testing and refinement!

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,12 @@
 var playerYVelocity = 0;
-var ballXVelocity = 5;
+
+var ballXVelocity;
+if(getRndInteger(0,2) == 1){
+  ballXVelocity = 3;
+}
+else{
+  ballXVelocity = -3;
+}
 var ballYVelocity = 0;
 
 var leftPlayerRight;
@@ -14,12 +21,16 @@ var ballRight;
 var ballTop;
 var ballBottom;
 
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
 document.addEventListener('keydown', function (event) {
   if (event.key == "ArrowUp") {
-    playerYVelocity = -5
+    playerYVelocity = -10
   }
   else if (event.key == "ArrowDown") {
-    playerYVelocity = 5
+    playerYVelocity = 10
   }
 });
 
@@ -40,13 +51,12 @@ var myGameArea = {
     this.canvas.height;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(periodic, 20);
+    this.interval = setInterval(periodic, 10);
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
-
 
 var leftPlayer;
 var rightPlayer;
@@ -57,9 +67,14 @@ var width = 30;
 
 function startGame() {
   myGameArea.start();
-  leftPlayer = new player(width, height, "black", 100, 500);
-  rightPlayer = new player(width, height, "black", 900, 500);
-  object = new ball(10, "red", 500, 500)
+  leftPlayer = new player(width, height, "white", 100, 500);
+  rightPlayer = new player(width, height, "white", 900, 500);
+  object = new ball(40, "white", 500, 500)
+}
+
+var extraSpeed = 0;
+function increaseSpeed(){
+  extraSpeed +=0.15;
 }
 
 function player(width, height, color, x, y, rot) {
@@ -107,7 +122,7 @@ function isContacting() {
   leftPlayerTop = leftPlayer.y + leftPlayer.height/2;
   leftPlayerBottom = leftPlayer.y - leftPlayer.height/2;
   rightPlayerLeft = rightPlayer.x - rightPlayer.width/2;
-  rightPlayerTop = rightPlayer.x + rightPlayer.height/2;
+  rightPlayerTop = rightPlayer.y + rightPlayer.height/2;
   rightPlayerBottom = rightPlayer.y - rightPlayer.height/2;
 
   ballLeft = object.x - object.size; //size = the radius
@@ -115,11 +130,45 @@ function isContacting() {
   ballTop = object.y + object.size;
   ballBottom = object.y - object.size
 
-  if (((ballLeft < leftPlayerRight) && (ballLeft > leftPlayerRight-10)) && ((ballTop > leftPlayerBottom) && (ballBottom < leftPlayerTop))) {
-    console.log("bounce on right")
+  //Is game over?
+  if (ballLeft < 0){
+    ballXVelocity=0;
+    ballYVelocity = 0;
+    extraSpeed = 0;
+    object.x = 500;
+    object.y = 500;
+    ballXVelocity = 2;
+    extraSpeed = 0;
   }
-  if (((ballRight < rightPlayerLeft) && ((ballRight < rightPlayerLeft+10))) && ((ballTop > rightPlayerBottom) && (ballBottom < rightPlayerTop))) {
-    console.log("bounce on left")
+  if (ballRight > 1000){
+    ballXVelocity=0;
+    ballYVelocity = 0;
+    object.x = 500;
+    object.y = 500;
+    ballXVelocity = 2;
+    extraSpeed = 0;
+  }
+
+  //Top and bottom walls
+  if ((ballBottom <= 0)){
+    ballYVelocity = getRndInteger(4,8);
+  }
+  if ((ballTop >= 1000)){
+    ballYVelocity = -getRndInteger(4,8);
+  }
+
+  if (((ballLeft < leftPlayerRight) && (ballLeft > leftPlayerRight-10)) && ((ballTop > leftPlayerBottom) && (ballBottom < leftPlayerTop))) {
+    increaseSpeed();
+    object.x = leftPlayerRight + object.size + 5
+    ballXVelocity = 6 + extraSpeed;
+    ballYVelocity = getRndInteger(-3,3)
+    
+  }
+  if (((ballRight > rightPlayerLeft) && ((ballRight < rightPlayerLeft+10))) && ((ballTop > rightPlayerBottom) && (ballBottom < rightPlayerTop))) {
+    increaseSpeed();
+    object.x = rightPlayerLeft - object.size - 5
+    ballXVelocity = -6 - extraSpeed;
+    ballYVelocity = getRndInteger(-3,3)
   }
 }
 
@@ -130,11 +179,10 @@ function periodic() {
   rightPlayer.y += playerYVelocity;
   rightPlayer.update();
   object.x += ballXVelocity;
+  object.y += ballYVelocity;
   object.update()
 
   isContacting();
 }
-
-
 
 startGame();
