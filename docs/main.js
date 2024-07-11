f43f32de6c31d41cdb0ae68485dbe1eb58a0bbf9
hjmcphail76 <hjmcphail@gmail.com>
2024-07-10 20:56:12 -0400

    Added collision detection

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,18 @@
 var playerYVelocity = 0;
 var ballXVelocity = -2;
 
+var leftPlayerRight;
+var leftPlayerTop;
+var leftPlayerBottom;
+var rightPlayerLeft;
+var rightPlayerTop;
+var rightPlayerBottom;
+
+var ballLeft;
+var ballRight;
+var ballTop;
+var ballBottom;
+
 document.addEventListener('keydown', function (event) {
   if (event.key == "ArrowUp") {
     playerYVelocity = -5
@@ -27,7 +39,7 @@ var myGameArea = {
     this.canvas.height;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
+    this.interval = setInterval(periodic, 20);
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -53,6 +65,7 @@ function player(width, height, color, x, y, rot) {
   this.width = width;
   this.rot = rot;
   this.height = height;
+  
   this.x = x;
   this.y = y;
   this.update = function () {
@@ -72,6 +85,8 @@ function ball(size, color, x, y) {
   this.x = x;
   this.y = y;
   this.color = color;
+  this.x = x;
+  this.y = y;
   this.update = function () {
     ctx = myGameArea.context;
     ctx.save();
@@ -83,33 +98,51 @@ function ball(size, color, x, y) {
   }
 
 }
+
+
 function isContacting() {
-  if ((object.x - 50) < (leftPlayer.x + width / 2)) { //Left Player Detection
-    //TODO:
+  //This function handles detection of contacting one of the paddles.
+  leftPlayerRight = leftPlayer.x + leftPlayer.width/2;
+  leftPlayerTop = leftPlayer.y + leftPlayer.height/2;
+  leftPlayerBottom = leftPlayer.y - leftPlayer.height/2;
+  rightPlayerLeft = rightPlayer.x - rightPlayer.width/2;
+  rightPlayerTop = rightPlayer.x + rightPlayer.height/2;
+  rightPlayerBottom = rightPlayer.y - rightPlayer.height/2;
 
+  ballLeft = object.x - object.size; //size = the radius
+  ballRight = object.x + object.size;
+  ballTop = object.y + object.size;
+  ballBottom = object.y - object.size
 
+  if (object.x < 500){ //If it is on the left side of the screen
+    if ((ballLeft < leftPlayerRight) && ((ballTop > leftPlayerBottom) && (ballBottom < leftPlayerTop))) {
+      return true;
+    }
+    return false
+  }
+  else{
+    if ((ballRight < rightPlayerLeft) && ((ballTop > rightPlayerBottom) && (ballBottom < rightPlayerTop))) {
+      return true;
+    }
+    return false
   }
 }
+function bounce(){
+  //TODO:
+}
 
-
-function updateGameArea() {
+function periodic() {
   myGameArea.clear();
-
   leftPlayer.y += playerYVelocity;
   leftPlayer.update();
-
   rightPlayer.y += playerYVelocity;
   rightPlayer.update();
-
-  object.x += ballXVelocity; //temp
+  object.x += ballXVelocity;
   object.update()
+
   isContacting();
 }
 
-function setSpeed() {
-  var mylist = document.getElementById("myList");
-  var result = mylist.selectedIndex.text;
-  console.log(result); //TODO:FIX THIS
-}
+
 
 startGame();
